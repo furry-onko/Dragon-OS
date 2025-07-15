a2b4c85f768b246ffd813cee04c54ccbfe1d8064
furry-onko <cute.neko.boy.uwu@gmail.com>
2025-07-15 16:26:49 +0200

    feat: implement logo printing and removed install() function because it's not necessary

--- main.py
+++ main.py
@@ -1,67 +1,107 @@
 # By Onko Aikuu :3
 import os
 import sys
-os.system("cls" if os.name == "nt" else "clear")
-os.system("py -m pip install --upgrade pip")
 import subprocess
 
-os.chdir(os.path.dirname(__file__))
-os.system("title Dragon OS - Loading Libraries...")
-os.system("cls" if os.name == "nt" else "clear")
+def printLogo() -> None:    
+    print("⢗⣝⢮⡳⣝⢮⡳⣝⢮⡳⣝⢮⡳⣝⢮⢯⡺⣝⢮⢯⡺⣝⣞⢽⣪⢟⡮⡯⣞⢽⢵⣫⢯⢞⡽⣝⣞⢽⢝⣞⡽⣝⢽⡺⣽⡹⣝⡽⣝⣵".center(os.get_terminal_size().columns))
+    print("⢕⢗⡳⣝⢮⢳⣹⡪⡳⣝⢮⡳⣝⢮⢯⡺⣝⢮⣫⡳⡽⣕⣗⢯⢞⡵⡯⣞⡵⡯⣳⣳⡫⡯⣞⣗⢽⣝⢽⣺⡺⡽⡵⣻⣺⡺⣵⡻⣺⣺".center(os.get_terminal_size().columns))
+    print("⡺⣝⢞⢮⡳⣝⢮⡺⣝⢮⡳⣝⢮⢯⡺⣝⢮⣳⡳⡽⣝⣞⢮⢯⣫⢾⢝⡮⡯⣞⣗⢗⡽⣝⣞⢮⣗⢗⡯⣞⣞⡽⣝⣞⣞⣞⡵⣯⣳⣳".center(os.get_terminal_size().columns))
+    print("⡞⡮⣫⢞⢞⢮⡳⣝⢮⡳⣝⡮⡯⣳⢽⡺⣵⡳⡮⣙⠺⢪⢯⣳⡹⢽⢝⡾⣝⣞⡮⡯⡯⣞⡮⣗⣗⢯⢯⣞⣞⢾⢵⣳⣳⡳⣽⣺⣺⣪".center(os.get_terminal_size().columns))
+    print("⢮⡫⡮⣫⣫⡳⣝⢮⣳⢽⣕⢯⢯⣞⣗⠽⡮⡯⡯⡯⡯⣦⣈⡘⠺⠳⣍⠚⢗⢷⢽⢽⣝⣮⣻⡺⡮⡯⣗⣗⣗⣯⣻⣺⣪⢯⣞⣞⣞⣮".center(os.get_terminal_size().columns))
+    print("⢕⢯⡫⣞⢮⢞⡚⠓⠓⠓⢹⡽⠁⢳⡳⡯⡶⣬⣉⠋⠫⠗⣯⡷⣷⣔⡤⡀⡀⠙⠹⢵⣳⣳⣳⢽⢽⢽⣺⣺⣺⣺⣺⣺⣺⢽⣺⣺⣺⣺".center(os.get_terminal_size().columns))
+    print("⡹⣕⢯⢞⡽⣕⢯⣫⢏⣯⣳⣫⢯⢗⡯⡯⣻⣺⣞⣿⣶⣦⣄⡈⡉⠙⠻⠿⣾⣵⣰⡀⠄⠁⠋⠫⢯⣻⣺⣺⢞⣞⣞⣞⢾⣝⣞⣞⣞⢾".center(os.get_terminal_size().columns))
+    print("⡪⣗⢯⡳⣝⡮⡯⣺⢝⡮⣺⢼⢝⣵⣋⣋⣉⣉⣉⣉⣁⣉⣟⣿⣷⣧⣣⣢⣠⣀⢉⠙⠛⠻⢶⣦⡄⣀⠈⢞⣯⣗⣗⡯⣗⣗⣗⡷⡽⡽".center(os.get_terminal_size().columns))
+    print("⢽⣪⢗⡯⣳⢝⣞⡵⡯⣺⡳⣽⣽⣾⣻⣯⡿⠏⠻⢟⣿⣭⣉⡉⡈⡉⡙⡛⡙⠛⠛⠛⠛⠒⠊⠺⡷⣖⡿⠛⠚⠚⠚⠙⠚⠚⢑⡿⡽⣽".center(os.get_terminal_size().columns))
+    print("⣳⠳⠫⣞⡗⠯⠞⣞⡽⠾⢿⢻⣷⡟⠉⢀⢀⢄⣮⣾⢿⢽⣻⣟⡷⠿⠿⠿⠿⠿⠻⠟⢷⢗⣧⡷⣮⣯⢴⣤⣦⣴⣤⡔⢀⡴⡽⣞⡯⡷".center(os.get_terminal_size().columns))
+    print("⣳⣲⣲⡳⣳⡲⣲⢽⢵⣲⣽⢿⡷⠁⢔⢑⣬⣾⢗⡿⣿⣝⣝⡛⣻⣷⢷⡷⣷⣻⡾⠋⠁⠀⡀⠉⠛⠺⢟⣷⣻⣞⣷⣳⢯⡯⣯⢷⣻⢽".center(os.get_terminal_size().columns))
+    print("⢮⣺⢼⡺⣕⢯⣳⡫⣗⡷⣿⡛⠁⢌⢢⣾⢿⣺⡳⠁⠀⠉⢳⣿⣻⢽⢽⡽⠋⡁⣄⣆⣶⣼⢾⡾⣮⣖⣔⢄⠅⠛⢚⡽⣯⢯⡷⣻⣞⣯".center(os.get_terminal_size().columns))
+    print("⣳⡳⣝⢮⢯⡳⣳⢽⢵⣿⣻⡇⠌⣢⣾⡿⣝⢟⠻⠷⣦⣤⣿⣳⢽⢽⣕⣶⣻⣻⢽⡽⣳⡽⡽⣞⣗⣯⢯⢇⣱⢾⡽⡯⣯⠏⣯⣗⣗⡷".center(os.get_terminal_size().columns))
+    print("⣗⢽⡺⣝⡵⣫⢾⢕⣿⢾⣻⠎⣸⣯⣿⣻⢪⠮⠊⠀⠀⢸⡾⣺⢽⣳⣳⣳⢗⡯⣟⣞⡷⡯⣟⣗⣟⣞⣷⣻⢽⣻⣺⢯⢯⠇⣗⣷⡳⡯".center(os.get_terminal_size().columns))
+    print("⢷⢝⡾⣕⢯⣳⡫⡯⣾⣿⣻⡼⣿⠹⣾⣗⣿⡶⠶⠶⠶⠾⡯⡯⣟⣞⣗⡯⣯⢯⢷⣳⢯⢿⢵⣻⢮⣗⣷⣫⡯⡷⣯⣻⡝⠀⣿⢮⢯⢯".center(os.get_terminal_size().columns))
+    print("⢽⢵⣫⢾⢝⡮⣞⡽⣽⡾⣯⣟⠣⢀⢻⣽⣞⢖⡀⡀⠀⣀⣽⢯⣟⢾⢵⣻⢽⢽⣻⣺⢯⣻⡽⣞⣟⣞⣾⣺⢽⣽⣺⢾⠁⢠⣿⢽⢽⢽".center(os.get_terminal_size().columns))
+    print("⣹⢵⣳⡫⣗⡽⡮⣻⣺⢿⣯⢿⣻⡆⡂⣻⣷⢫⣧⡶⠟⠋⠀⠈⣫⡿⡽⣽⢽⣻⣺⢾⢽⣳⢯⣗⣟⡾⣺⢾⢽⣺⢾⠋⡀⣾⢯⢯⢯⣻".center(os.get_terminal_size().columns))
+    print("⢮⣳⡳⡽⣕⢯⢯⢞⣞⣿⣽⣟⣷⣻⡔⢄⢿⣿⣳⣝⢆⠀⢠⡾⠏⠀⠉⣯⠟⠾⣽⡽⡽⠾⣽⠚⠁⠈⢯⣯⣟⠃⡟⠠⣼⡿⡽⡽⣽⣺".center(os.get_terminal_size().columns))
+    print("⢳⡳⡽⣝⣞⡽⡽⣝⣞⢞⣞⣯⣷⢿⡽⣖⢆⠻⣽⣾⣝⣾⣻⢕⡄⡀⣸⡟⠀⠀⢸⡇⠀⠀⢿⣇⢄⢢⣢⣷⡏⢀⢐⣾⣻⢽⢽⢽⣺⢾".center(os.get_terminal_size().columns))
+    print("⣝⡽⣝⣞⣞⣞⡽⣺⢮⡻⡮⣻⣽⢿⡽⣯⢿⡌⡊⡻⢽⣟⣮⣗⡽⣲⡿⣪⢦⡂⢸⣗⡲⡰⣜⣿⡼⣿⢽⡾⢐⣰⡾⣟⢾⢽⢽⡽⣞⣟".center(os.get_terminal_size().columns))
+    print("⡳⣽⣺⣺⣺⣺⣺⣳⣫⢯⠯⣳⣝⢿⣻⣽⢷⣻⡄⠢⡑⢌⢛⠿⣻⣿⣽⡾⣷⣽⢾⡷⣷⡿⠿⢚⢩⣰⡿⢡⣵⡿⡽⡽⡽⣽⢽⢾⣽⣺".center(os.get_terminal_size().columns))
+    print("⢽⡺⡦⣦⣢⢦⣤⣤⢼⣻⢄⡼⣺⣝⢿⣻⣿⡽⣟⣿⢶⢶⢴⣁⠢⡐⡩⢉⠍⡚⣫⣫⣷⢾⡽⣷⢿⣝⣼⡿⣻⣺⢽⢯⣻⡽⡽⣯⢾⣺".center(os.get_terminal_size().columns))
+    print("⢯⢯⢯⣞⢾⣝⣞⢾⢽⣺⢽⣝⢷⣝⢷⣝⡷⣟⣟⣿⣻⣿⣻⣯⡿⣷⢮⣶⣟⣿⣻⣽⡯⣿⣽⣟⣯⣟⣗⡯⣗⡯⣟⣽⣳⣻⡽⡯⣯⢷".center(os.get_terminal_size().columns))
+    print("⢽⢽⢵⡳⡯⣞⢾⢽⢽⣺⢽⣺⣳⣝⣗⣗⡯⣟⡾⡮⣗⡯⣟⡷⣿⣻⣯⣷⢿⢾⢯⡷⣻⣽⣺⣳⣳⣗⡯⣯⢯⣟⡽⣞⣗⣯⢯⡿⣽⣻".center(os.get_terminal_size().columns))
+    print("⡽⣝⣗⡯⡯⡯⡯⡯⣗⡯⣗⣗⣗⣗⣗⣗⡯⣗⡯⡯⣗⡿⡽⣽⣝⣗⡷⣽⢽⢽⣳⣻⣳⣗⣯⣞⣗⡷⡯⡿⡽⣞⣯⣟⡾⡽⡯⣟⣗⣟".center(os.get_terminal_size().columns))
+    print()
+    print("    ____                               ____  _____".center(os.get_terminal_size().columns))
+    print("   / __ \_________ _____ _____  ____  / __ \/ ___/".center(os.get_terminal_size().columns))
+    print("  / / / / ___/ __ `/ __ `/ __ \/ __ \/ / / /\__ \ ".center(os.get_terminal_size().columns))
+    print(" / /_/ / /  / /_/ / /_/ / /_/ / / / / /_/ /___/ / ".center(os.get_terminal_size().columns))
+    print("/_____/_/   \__,_/\__, /\____/_/ /_/\____//____/  ".center(os.get_terminal_size().columns))
+    print("                 /____/                           ".center(os.get_terminal_size().columns))
+    print("\n")
 
-print("⢗⣝⢮⡳⣝⢮⡳⣝⢮⡳⣝⢮⡳⣝⢮⢯⡺⣝⢮⢯⡺⣝⣞⢽⣪⢟⡮⡯⣞⢽⢵⣫⢯⢞⡽⣝⣞⢽⢝⣞⡽⣝⢽⡺⣽⡹⣝⡽⣝⣵".center(os.get_terminal_size().columns))
-print("⢕⢗⡳⣝⢮⢳⣹⡪⡳⣝⢮⡳⣝⢮⢯⡺⣝⢮⣫⡳⡽⣕⣗⢯⢞⡵⡯⣞⡵⡯⣳⣳⡫⡯⣞⣗⢽⣝⢽⣺⡺⡽⡵⣻⣺⡺⣵⡻⣺⣺".center(os.get_terminal_size().columns))
-print("⡺⣝⢞⢮⡳⣝⢮⡺⣝⢮⡳⣝⢮⢯⡺⣝⢮⣳⡳⡽⣝⣞⢮⢯⣫⢾⢝⡮⡯⣞⣗⢗⡽⣝⣞⢮⣗⢗⡯⣞⣞⡽⣝⣞⣞⣞⡵⣯⣳⣳".center(os.get_terminal_size().columns))
-print("⡞⡮⣫⢞⢞⢮⡳⣝⢮⡳⣝⡮⡯⣳⢽⡺⣵⡳⡮⣙⠺⢪⢯⣳⡹⢽⢝⡾⣝⣞⡮⡯⡯⣞⡮⣗⣗⢯⢯⣞⣞⢾⢵⣳⣳⡳⣽⣺⣺⣪".center(os.get_terminal_size().columns))
-print("⢮⡫⡮⣫⣫⡳⣝⢮⣳⢽⣕⢯⢯⣞⣗⠽⡮⡯⡯⡯⡯⣦⣈⡘⠺⠳⣍⠚⢗⢷⢽⢽⣝⣮⣻⡺⡮⡯⣗⣗⣗⣯⣻⣺⣪⢯⣞⣞⣞⣮".center(os.get_terminal_size().columns))
-print("⢕⢯⡫⣞⢮⢞⡚⠓⠓⠓⢹⡽⠁⢳⡳⡯⡶⣬⣉⠋⠫⠗⣯⡷⣷⣔⡤⡀⡀⠙⠹⢵⣳⣳⣳⢽⢽⢽⣺⣺⣺⣺⣺⣺⣺⢽⣺⣺⣺⣺".center(os.get_terminal_size().columns))
-print("⡹⣕⢯⢞⡽⣕⢯⣫⢏⣯⣳⣫⢯⢗⡯⡯⣻⣺⣞⣿⣶⣦⣄⡈⡉⠙⠻⠿⣾⣵⣰⡀⠄⠁⠋⠫⢯⣻⣺⣺⢞⣞⣞⣞⢾⣝⣞⣞⣞⢾".center(os.get_terminal_size().columns))
-print("⡪⣗⢯⡳⣝⡮⡯⣺⢝⡮⣺⢼⢝⣵⣋⣋⣉⣉⣉⣉⣁⣉⣟⣿⣷⣧⣣⣢⣠⣀⢉⠙⠛⠻⢶⣦⡄⣀⠈⢞⣯⣗⣗⡯⣗⣗⣗⡷⡽⡽".center(os.get_terminal_size().columns))
-print("⢽⣪⢗⡯⣳⢝⣞⡵⡯⣺⡳⣽⣽⣾⣻⣯⡿⠏⠻⢟⣿⣭⣉⡉⡈⡉⡙⡛⡙⠛⠛⠛⠛⠒⠊⠺⡷⣖⡿⠛⠚⠚⠚⠙⠚⠚⢑⡿⡽⣽".center(os.get_terminal_size().columns))
-print("⣳⠳⠫⣞⡗⠯⠞⣞⡽⠾⢿⢻⣷⡟⠉⢀⢀⢄⣮⣾⢿⢽⣻⣟⡷⠿⠿⠿⠿⠿⠻⠟⢷⢗⣧⡷⣮⣯⢴⣤⣦⣴⣤⡔⢀⡴⡽⣞⡯⡷".center(os.get_terminal_size().columns))
-print("⣳⣲⣲⡳⣳⡲⣲⢽⢵⣲⣽⢿⡷⠁⢔⢑⣬⣾⢗⡿⣿⣝⣝⡛⣻⣷⢷⡷⣷⣻⡾⠋⠁⠀⡀⠉⠛⠺⢟⣷⣻⣞⣷⣳⢯⡯⣯⢷⣻⢽".center(os.get_terminal_size().columns))
-print("⢮⣺⢼⡺⣕⢯⣳⡫⣗⡷⣿⡛⠁⢌⢢⣾⢿⣺⡳⠁⠀⠉⢳⣿⣻⢽⢽⡽⠋⡁⣄⣆⣶⣼⢾⡾⣮⣖⣔⢄⠅⠛⢚⡽⣯⢯⡷⣻⣞⣯".center(os.get_terminal_size().columns))
-print("⣳⡳⣝⢮⢯⡳⣳⢽⢵⣿⣻⡇⠌⣢⣾⡿⣝⢟⠻⠷⣦⣤⣿⣳⢽⢽⣕⣶⣻⣻⢽⡽⣳⡽⡽⣞⣗⣯⢯⢇⣱⢾⡽⡯⣯⠏⣯⣗⣗⡷".center(os.get_terminal_size().columns))
-print("⣗⢽⡺⣝⡵⣫⢾⢕⣿⢾⣻⠎⣸⣯⣿⣻⢪⠮⠊⠀⠀⢸⡾⣺⢽⣳⣳⣳⢗⡯⣟⣞⡷⡯⣟⣗⣟⣞⣷⣻⢽⣻⣺⢯⢯⠇⣗⣷⡳⡯".center(os.get_terminal_size().columns))
-print("⢷⢝⡾⣕⢯⣳⡫⡯⣾⣿⣻⡼⣿⠹⣾⣗⣿⡶⠶⠶⠶⠾⡯⡯⣟⣞⣗⡯⣯⢯⢷⣳⢯⢿⢵⣻⢮⣗⣷⣫⡯⡷⣯⣻⡝⠀⣿⢮⢯⢯".center(os.get_terminal_size().columns))
-print("⢽⢵⣫⢾⢝⡮⣞⡽⣽⡾⣯⣟⠣⢀⢻⣽⣞⢖⡀⡀⠀⣀⣽⢯⣟⢾⢵⣻⢽⢽⣻⣺⢯⣻⡽⣞⣟⣞⣾⣺⢽⣽⣺⢾⠁⢠⣿⢽⢽⢽".center(os.get_terminal_size().columns))
-print("⣹⢵⣳⡫⣗⡽⡮⣻⣺⢿⣯⢿⣻⡆⡂⣻⣷⢫⣧⡶⠟⠋⠀⠈⣫⡿⡽⣽⢽⣻⣺⢾⢽⣳⢯⣗⣟⡾⣺⢾⢽⣺⢾⠋⡀⣾⢯⢯⢯⣻".center(os.get_terminal_size().columns))
-print("⢮⣳⡳⡽⣕⢯⢯⢞⣞⣿⣽⣟⣷⣻⡔⢄⢿⣿⣳⣝⢆⠀⢠⡾⠏⠀⠉⣯⠟⠾⣽⡽⡽⠾⣽⠚⠁⠈⢯⣯⣟⠃⡟⠠⣼⡿⡽⡽⣽⣺".center(os.get_terminal_size().columns))
-print("⢳⡳⡽⣝⣞⡽⡽⣝⣞⢞⣞⣯⣷⢿⡽⣖⢆⠻⣽⣾⣝⣾⣻⢕⡄⡀⣸⡟⠀⠀⢸⡇⠀⠀⢿⣇⢄⢢⣢⣷⡏⢀⢐⣾⣻⢽⢽⢽⣺⢾".center(os.get_terminal_size().columns))
-print("⣝⡽⣝⣞⣞⣞⡽⣺⢮⡻⡮⣻⣽⢿⡽⣯⢿⡌⡊⡻⢽⣟⣮⣗⡽⣲⡿⣪⢦⡂⢸⣗⡲⡰⣜⣿⡼⣿⢽⡾⢐⣰⡾⣟⢾⢽⢽⡽⣞⣟".center(os.get_terminal_size().columns))
-print("⡳⣽⣺⣺⣺⣺⣺⣳⣫⢯⠯⣳⣝⢿⣻⣽⢷⣻⡄⠢⡑⢌⢛⠿⣻⣿⣽⡾⣷⣽⢾⡷⣷⡿⠿⢚⢩⣰⡿⢡⣵⡿⡽⡽⡽⣽⢽⢾⣽⣺".center(os.get_terminal_size().columns))
-print("⢽⡺⡦⣦⣢⢦⣤⣤⢼⣻⢄⡼⣺⣝⢿⣻⣿⡽⣟⣿⢶⢶⢴⣁⠢⡐⡩⢉⠍⡚⣫⣫⣷⢾⡽⣷⢿⣝⣼⡿⣻⣺⢽⢯⣻⡽⡽⣯⢾⣺".center(os.get_terminal_size().columns))
-print("⢯⢯⢯⣞⢾⣝⣞⢾⢽⣺⢽⣝⢷⣝⢷⣝⡷⣟⣟⣿⣻⣿⣻⣯⡿⣷⢮⣶⣟⣿⣻⣽⡯⣿⣽⣟⣯⣟⣗⡯⣗⡯⣟⣽⣳⣻⡽⡯⣯⢷".center(os.get_terminal_size().columns))
-print("⢽⢽⢵⡳⡯⣞⢾⢽⢽⣺⢽⣺⣳⣝⣗⣗⡯⣟⡾⡮⣗⡯⣟⡷⣿⣻⣯⣷⢿⢾⢯⡷⣻⣽⣺⣳⣳⣗⡯⣯⢯⣟⡽⣞⣗⣯⢯⡿⣽⣻".center(os.get_terminal_size().columns))
-print("⡽⣝⣗⡯⡯⡯⡯⡯⣗⡯⣗⣗⣗⣗⣗⣗⡯⣗⡯⡯⣗⡿⡽⣽⣝⣗⡷⣽⢽⢽⣳⣻⣳⣗⣯⣞⣗⡷⡯⡿⡽⣞⣯⣟⡾⡽⡯⣟⣗⣟".center(os.get_terminal_size().columns))
-print()
-print("    ____                               ____  _____".center(os.get_terminal_size().columns))
-print("   / __ \_________ _____ _____  ____  / __ \/ ___/".center(os.get_terminal_size().columns))
-print("  / / / / ___/ __ `/ __ `/ __ \/ __ \/ / / /\__ \ ".center(os.get_terminal_size().columns))
-print(" / /_/ / /  / /_/ / /_/ / /_/ / / / / /_/ /___/ / ".center(os.get_terminal_size().columns))
-print("/_____/_/   \__,_/\__, /\____/_/ /_/\____//____/  ".center(os.get_terminal_size().columns))
-print("                 /____/                           ".center(os.get_terminal_size().columns))
-print("\n")
+if os.name == "nt":
+    os.system("cls")
+    os.system("py -m pip install --upgrade pip")
+    os.chdir(
+        os.path.dirname(__file__)
+    )
 
-print("Loading... ".center(os.get_terminal_size().columns))
+    os.system("title Dragon OS - Loading Libraries...")
+    os.system("cls")
+    printLogo()
 
-dependencies: list = ["colorama", "keyboard", "requests", "bcrypt",  "windows-curses"]
+else:
+    os.system("clear")
+    os.system("python3 -m pip install --upgrade pip --break-system-packages")
+    os.chdir(
+        os.path.dirname(__file__)
+    )
+
+    print("\033]0;Dragon OS - Loading Libraries...\007")
+    os.system("clear")
+    printLogo()
+
+
+print(
+    "Loading... "
+    .center(
+        os.get_terminal_size().columns
+    )
+)
+
+dependencies: list = ["colorama", "keyboard", "requests", "bcrypt"]
+
+if os.name == "nt": dependencies.append("windows-curses")
+
+# sys.executable
 
 for i, package in enumerate(dependencies):
     try:
-        os.system(f"title Dragon OS - Loading Libraries... {i +1}/{len(dependencies)}")
+        if os.name == "nt":
+            os.system(f"title Dragon OS - Loading Libraries... {i +1}/{len(dependencies)}")
+        else:
+            print(f"\033]0;Dragon OS - Loading Libraries... {i +1}/{len(dependencies)}")
+
         print(f"[\033[34mINFO\033[0m]      Loading libraries {i +1}/{len(dependencies)} ({package})", end="\r")
-        subprocess.call([sys.executable, "-m", "pip", "install", package], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.call([sys.executable, "-m", "pip", "install", package, "--break-system-packages"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         print(" " * 50, end="\r")
 
     except:
         print(f"[\033[0;31mERROR\033[0m]      Error loading library: {package}")
 
 print("[\033[34mINFO\033[0m]      Loading libraries - Done")
-os.system("title Dragon OS - Loading Libraries... - Done")
 
-print("[\033[34mINFO\033[0m]      Importing libraries", end="\r")
-os.system("title Dragon OS - Importing Libraries...")
+if os.name == "nt":
+    os.system("title Dragon OS - Loading Libraries... - Done")
+else:
+    print("\033]0;Dragon OS - Loading Libraries... - Done")
+
+print("[[\033[34mINFO\033[0m]      Importing libraries", end="\r")
+
+if os.name == "nt":
+    os.system("title Dragon OS - Importing Libraries...")
+else:
+    print("\033]0;Dragon OS - Importing Libraries...\007")
+
 from Apps import dragoninstall as drg_inst
 from Apps import datetime as dt
 from Apps import dragonconfig as dc
@@ -76,56 +116,17 @@ import json
 
 print(" " * 50, end="\r")
 print("[\033[34mINFO\033[0m]      Importing libraries - Done")
-os.system("title Dragon OS - Importing Libraries... - Done")
+
+if os.name == "nt":
+    os.system("title Dragon OS - Importing Libraries... - Done")
+else:
+    print("\033]0;Dragon OS - Importing Libraries... - Done\007")
 
 init(autoreset=True)
 
 user: str = ""
 with open("Files/config/core.json", "r") as f:
-    data: dict | list = json.load(f)
-    user = data["CurrentUser"]
-    f.close()
-
-def download() -> None:
-    try:
-        headers: dict = {"User-Agent": "Mozilla/5.0"}
-        download_core_link: str = "https://raw.githubusercontent.com/furry-onko/Dragon-OS/refs/heads/main/Files/config/core.json"
-        download_users_link: str = "https://raw.githubusercontent.com/furry-onko/Dragon-OS/refs/heads/main/Files/config/users.json"
-        download_dragoninstall_link: str = "https://raw.githubusercontent.com/furry-onko/Dragon-OS/refs/heads/main/Apps/dragoninstall.py" 
-
-        download_core_response = requests.get(download_core_link, headers=headers)
-        download_core_response.raise_for_status()
-
-        download_users_response = requests.get(download_users_link, headers=headers)
-        download_users_response.raise_for_status()
-
-        download_dragoninstall_response = requests.get(download_dragoninstall_link, headers=headers)
-        download_dragoninstall_response.raise_for_status()
-
-        os.makedirs("Files/config", exist_ok=True)
-        os.chdir("Files/config")
-        if not os.path.exists("core.json") and not os.path.exists("users.json"):        
-            with open("core.json", "wb") as f:
-                f.write(download_core_response.content)
-                f.close()
-        
-            with open("users.json", "wb") as f:
-                f.write(download_users_response.content)
-                f.close()
-        
-        
-        os.chdir("../..")
-        
-        if not os.path.exists("Apps/dragoninstall.py"):
-            os.makedirs("Apps", exist_ok=True)
-            os.chdir("Apps")
-            with open("dragoninstall.py", "wb") as f:
-                f.write(download_dragoninstall_response.content)
-                f.close()
-    except Exception as x:
-        print(f"ERROR DOWNLOADING FILES: {x}")
-        print("PLEASE CHECK YOUR INTERNET CONNECTION")
-        print("OR CHECK YOUR FIREWALL SETTINGS")
+    user: str = json.load(f)["CurrentUser"]
 
 def terminal() -> None:
     global user
@@ -133,7 +134,6 @@ def terminal() -> None:
 
     def clear() -> None:
         os.system("cls" if os.name == "nt" else "clear")
-        # print(f"${user}: ", end="")
 
     keyboard.add_hotkey("ctrl+l", clear)
 
@@ -225,6 +225,9 @@ def terminal() -> None:
             print("user", end="  ")
             print("dragonconfig")
 
-os.system(f"title Dragon OS - ${user}")
-download()
+if os.name == "nt":
+    os.system(f"title Dragon OS - ${user}")
+else:
+    print(f"\033]0;Dragon OS - ${user}\007")
+
 terminal()
